4f8f324b53aa456c8cd5d044e6b992eab888e89a
zhanghui <hui.zhang@bangdao-tech.com>
2019-04-08 15:36:30 +0800

    xx

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,42 +1,25 @@
-import com.xinwu.controller.HelloController;
-import org.junit.Before;
+import com.xinwu.Application;
+import com.xinwu.common.dal.dao.StudentDao;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author: 张辉
  * @create: 2019-04-08
  **/
-@SpringBootTest(classes = MockServletContext.class)
+@SpringBootTest(classes = Application.class)
 @RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
 public class ApplicationTest {
 
-    private MockMvc mockMvc;
-
-    @Before
-    public void init(){
-        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
-    }
+    @Autowired
+    private StudentDao studentDao;
 
     @Test
-    public void helloTest() throws Exception{
-        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("hello world")));
-
+    public void selectByIdTest(){
+        System.out.println(studentDao.selectById(1));
     }
 
 }
---
+++ src/test/java/WebApplicationTest.java
@@ -0,0 +1,42 @@
+import com.xinwu.controller.HelloController;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author: 张辉
+ * @create: 2019-04-08
+ **/
+@SpringBootTest(classes = MockServletContext.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class WebApplicationTest {
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void init(){
+        mockMvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+
+    @Test
+    public void helloTest() throws Exception{
+        mockMvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello world")));
+
+    }
+
+}
