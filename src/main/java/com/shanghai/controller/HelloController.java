1d9e3a5fcb21ae3c88bb6abf9e73efdc253dccf7
zhanghui <hui.zhang@bangdao-tech.com>
2019-04-08 15:07:18 +0800

    测试controller

---
+++ src/main/java/com/shanghai/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.shanghai.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: 张辉
+ * @create: 2019-04-08
+ **/
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String index(){
+        return "hello world";
+    }
+
+}
