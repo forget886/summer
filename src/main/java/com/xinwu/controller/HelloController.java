227ba1c9cf220a6d6114884c85acb2ba843cb463
zhanghui <hui.zhang@bangdao-tech.com>
2019-06-17 11:08:33 +0800

    Update HelloController.java

--- src/main/java/com/xinwu/controller/HelloController.java
+++ src/main/java/com/xinwu/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.xinwu.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
 /**
  * @author: 张辉
  * @create: 2019-04-08
@@ -12,8 +15,18 @@ public class HelloController {
 
 
     @RequestMapping(value = "/hello")
-    public String index() {
+    public String index(HttpServletRequest request) {
+        System.out.println(request.getHeader("Referer"));
+        System.out.println(request.getHeader("Origin"));
+        System.out.println(request.getHeader("User-Agent"));
+        Enumeration<String> names = request.getHeaderNames();
+        if(names.hasMoreElements()){
+            String name = names.nextElement();
+            System.out.println(name + " : " + request.getHeader(name));
+        }
         return "hello world";
     }
 
 }
+
+
